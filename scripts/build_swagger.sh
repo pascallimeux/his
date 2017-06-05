39269dc3c3c86df4f4b89e349418c83a4a55583e
orange <orange@hyperledger1.rd.francetelecom.fr>
2017-06-05 14:28:05 +0200

    add swagger dependency

--- scripts/build_swagger.sh
+++ scripts/build_swagger.sh
@@ -6,10 +6,10 @@ swagger init spec \
     --title "his" \
     --description "Hyperledger Interface Server" \
     --version 1.0.0 \
-    --scheme http  > /dev/null 2>&1
-swagger generate spec -o ./swagger.json -i ./swagger.yml  > /dev/null 2>&1
+    --scheme http  > /dev/null 2>&1 
+swagger generate spec -o ./swagger.json -i ./swagger.yml  > /dev/null  2>&1
 go get -u -f ./...
-swagger generate server -f ./swagger.json -A his > /dev/null 2>&1
+swagger generate server -f ./swagger.json -A his > /dev/null 
 if ls $PROJECTPATH/swagger.* 1> /dev/null 2>&1; then
     echo "swagger files created (swagger.yml swagger.json)"
 else
--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,3 +3,7 @@ echo "> Installing dependencies..."
 go get -u github.com/kardianos/govendor
 go get github.com/gorilla/mux
 go get github.com/op/go-logging
+go get  -u github.com/go-swagger/go-swagger/cmd/swagger
+cd $GOPATH/src/github.com/go-swagger/go-swagger/cmd/swagger
+go build swagger.go
+sudo cp swagger /usr/local/bin
